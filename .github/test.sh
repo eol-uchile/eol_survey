e09294f9f13c25678a8d77f3769b921af50df091
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-05-05 16:03:37 -0400

    chore: adds genbadge coverage to test.sh & updates setup.cfg

--- .github/test.sh
+++ .github/test.sh
@@ -1,18 +1,17 @@
-#!/bin/bash
-
 set -e
 
 pip install --src /openedx/venv/src -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
 
 cd /openedx/requirements/app
-cp /openedx/edx-platform/setup.cfg .
 
 mkdir test_root
-cd test_root/
-ln -s /openedx/staticfiles .
+ln -s /openedx/staticfiles ./test_root/
 
 cd /openedx/requirements/app
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eol_survey/tests.py \
-  && \
-  rm -rf test_root
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eol_survey/tests.py
+
+rm -rf test_root
+
+genbadge coverage
